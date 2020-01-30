ec2e739e4fd86eb9f5bae30ad1449265dc6efd20
tyler9937 <tyler9937@users.noreply.github.com>
2020-01-30 18:38:38 -0500

    update

--- tut/__init__.py
+++
@@ -1,6 +0,0 @@
-from .api import create_app
-
-APP = create_app()
-
-#gunicorn twitoff:APP -t 1200
-#FLASK_APP=twitoff:APP flask run
--- tut/api.py
+++ tut/api.py
@@ -5,44 +5,42 @@ import traceback
 import pandas as pd
 import numpy as np
 
-def create_app():
+# Your API definition
+app = Flask(__name__)
 
-    # Your API definition
-    app = Flask(__name__)
+@app.route('/')
+def hello():
+    return 'Hello Welcome To My Predictive Model'
 
-    @app.route('/')
-    def hello():
-        return 'Hello Welcome To My Predictive Model'
-
-    @app.route('/predict', methods=['POST'])
-    def predict():
-        if lr:
-            try:
-                json_ = request.json
-                print(json_)
-                query = pd.get_dummies(pd.DataFrame(json_))
-                query = query.reindex(columns=model_columns, fill_value=0)
-
-                prediction = list(lr.predict(query))
-
-                return jsonify({'prediction': str(prediction)})
-
-            except:
-
-                return jsonify({'trace': traceback.format_exc()})
-        else:
-            print ('Train the model first')
-            return ('No model here to use')
-
-    if __name__ == '__main__':
+@app.route('/predict', methods=['POST'])
+def predict():
+    if lr:
         try:
-            port = int(sys.argv[1]) # This is for a command-line input
+            json_ = request.json
+            print(json_)
+            query = pd.get_dummies(pd.DataFrame(json_))
+            query = query.reindex(columns=model_columns, fill_value=0)
+
+            prediction = list(lr.predict(query))
+
+            return jsonify({'prediction': str(prediction)})
+
         except:
-            port = 12345 # If you don't provide any port the port will be set to 12345
 
-        lr = joblib.load("model.pkl") # Load "model.pkl"
-        print ('Model loaded')
-        model_columns = joblib.load("model_columns.pkl") # Load "model_columns.pkl"
-        print ('Model columns loaded')
+            return jsonify({'trace': traceback.format_exc()})
+    else:
+        print ('Train the model first')
+        return ('No model here to use')
 
-        app.run(port=port, debug=True)
+if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1]) # This is for a command-line input
+    except:
+        port = 12345 # If you don't provide any port the port will be set to 12345
+
+    lr = joblib.load("model.pkl") # Load "model.pkl"
+    print ('Model loaded')
+    model_columns = joblib.load("model_columns.pkl") # Load "model_columns.pkl"
+    print ('Model columns loaded')
+
+    app.run(port=port, debug=True)
